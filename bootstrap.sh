2cb3af7caf2f52c1d4943f311b6a1ed1397c68e3
Maxime Labelle <springcomp@users.noreply.github.com>
2022-11-24 15:42:09 +0100

    Support GitHub Codespaces

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,15 +3,24 @@
 ## install dependencies
 ## ====================
 
-sudo apt-get update
-sudo apt-get install python3 python3-pip -y
-sudo ln -s /usr/bin/pip3 /usr/local/bin/pip
-pip install --user git+ssh://git@github.com/Lokaltog/powerline
+if [[ $CODESPACES != "true" ]];
+then
+
+    sudo apt-get update
+    sudo apt-get install python3 python3-pip -y
+    sudo ln -s /usr/bin/pip3 /usr/local/bin/pip
+
+    wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+fi
+
+[[ -d ~/.ssh ]] || mkdir ~/.ssh
+curl --silent https://api.github.com/meta | jq --raw-output '"github.com "+.ssh_keys[]' >>~/.ssh/known_hosts
+
+pip install --user git+https://git@github.com/powerline/powerline
 pip install --user powerline-gitstatus
 
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
-
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+[[ -d ~/.vim/bundle/Vundle.vim ]] || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 ## load custom ~/.bashrc_xxx scripts
 ## =================================
@@ -32,5 +41,4 @@ EOF
 ## reload
 ## ======
 
-. .bashrc
-
+. ~/.bashrc
