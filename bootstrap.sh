21a9ed730e22578c765d2227b71521d2c5bcb884
Springcomp <springcomp@users.noreply.github.com>
2023-04-04 13:33:55 +0200

    [powerline] Updated powerline setup

--- bootstrap.sh
+++ bootstrap.sh
@@ -8,7 +8,9 @@ then
 
     sudo apt-get update
     sudo apt-get install python3 python3-pip -y
-    sudo ln -s /usr/bin/pip3 /usr/local/bin/pip
+    sudo apt-get install python-is-python3 -y
+    # [[ ! -L /usr/local/bin/pip ]] || sudo ln -s /usr/bin/pip3 /usr/local/bin/pip
+    # [[ ! -L /usr/local/bin-python ]] || sudo ln -s /usr/bin/python3 /usr/local/bin/python
 
     wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
 
@@ -17,13 +19,15 @@ fi
 [[ -d ~/.ssh ]] || mkdir ~/.ssh
 curl --silent https://api.github.com/meta | jq --raw-output '"github.com "+.ssh_keys[]' >>~/.ssh/known_hosts
 
-pip install --user git+https://git@github.com/powerline/powerline
+pip install --user git+ssh://git@github.com/powerline/powerline
 pip install --user powerline-gitstatus
 
 [[ -d ~/.vim/bundle/Vundle.vim ]] || git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 [[ -d ~/.vim/bundle/vim-monokai-pro ]] || git clone https://github.com/phanviet/vim-monokai-pro.git ~/.vim/bundle/vim-monokai-pro
+[[ -d ~/.tmux/plugins/tpm ]] || git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 vim +PluginInstall +qa
+
 
 ## load custom ~/.bashrc_xxx scripts
 ## =================================
